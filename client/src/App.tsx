97f6c777abf2e337933b2f29377f9fea16650f45
adriancc95 <adrian.castanoc@gmail.com>
2025-11-18 14:10:38 -0500

    Here we go again

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -34,11 +34,12 @@ import MobileSettings from "./pages/MobileSettings";
 import HubSpotSettings from "./pages/HubSpotSettings";
 import CompanyAdmin from "./pages/CompanyAdmin";
 import AnalyticsDashboard from "./pages/AnalyticsDashboard";
+import ProtectedRoute from "./components/ProtectedRoute"
+import Login from "./pages/Login"
 
 function Router() {
   return (
     <Switch>
-<<<<<<< HEAD
       <Route path={"/login"} component={Login} />
       <Route path={"/"} component={Home} />
 
@@ -172,35 +173,6 @@ function Router() {
           <Playbook />
         </ProtectedRoute>
       )} />
-=======
-      <Route path={"/"} component={Home} />
-      <Route path={"/customers"} component={Customers} />
-      <Route path={"/routes"} component={Routes} />
-      <Route path={"/visits"} component={Visits} />
-      <Route path={"/orders"} component={Orders} />
-      <Route path={"/products"} component={Products} />
-      <Route path={"/photos"} component={Photos} />
-      <Route path={"/tracking"} component={LiveTracking} />
-      <Route path={"/reports"} component={Reports} />
-      <Route path={"/customer-timeline/:customerId"} component={CustomerTimeline} />
-      <Route path={"/offline-settings"} component={OfflineSettings} />
-      <Route path={"/route-optimization"} component={RouteOptimization} />
-      <Route path="/distributors" component={Distributors} />
-      <Route path="/internal-notes" component={InternalNotesHistory} />
-      <Route path="/manager-dashboard" component={ManagerDashboard} />
-      <Route path={"/customers-map"} component={CustomersMap} />
-      <Route path={"/photo-gallery"} component={PhotoGallery} />
-      <Route path={"/predictive-analytics"} component={PredictiveAnalytics} />
-      <Route path={"/sales-coach"} component={SalesCoach} />
-      <Route path={"/mobile-settings"} component={MobileSettings} />
-      <Route path={"/hubspot-settings"} component={HubSpotSettings} />
-      <Route path={"/mileage-tracking"} component={MileageTracking} />
-      <Route path={"/mileage-reports"} component={MileageReports} />
-      <Route path="/analytics" component={AnalyticsDashboard} />
-       <Route path={"alerts"} component={Alerts} />
-      <Route path={"company-admin"} component={CompanyAdmin} />
-      <Route path={"/playbook"} component={Playbook} />
->>>>>>> parent of 4ba9cc1 (Add ProtectedRoute)
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
